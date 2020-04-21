5331d318d51bedb962000c9e7fa18a6fbcff2026
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2020-04-20 23:51:34 -0400

    Solved problem 1417

--- include/leetcode/problem_1417.hpp
+++ include/leetcode/problem_1417.hpp
@@ -9,32 +9,64 @@ namespace leetcode {
 static auto
 reformat(const std::string& s) -> std::string
 {
+  // Prelude
   using CharTy = typename std::string::value_type;
-
-  std::string result;
-  result.reserve(s.size());
-
-  auto characters = std::cbegin(s);
-  auto digits = std::cbegin(s);
-  do {
-    characters =
-      std::find_if(characters, std::cend(s), [](const CharTy& x) -> bool {
-        return std::isupper(x) || std::islower(x);
-      });
-    digits = std::find_if(digits, std::cend(s), [](const CharTy& x) -> bool {
+  auto next_characters = [&](const std::string::const_iterator characters) {
+    return std::find_if(characters, std::cend(s), [](const CharTy& x) -> bool {
+      return std::isupper(x) || std::islower(x);
+    });
+  };
+  auto next_digits = [&](const std::string::const_iterator digits) {
+    return std::find_if(digits, std::cend(s), [](const CharTy& x) -> bool {
       return std::isdigit(x);
     });
-    if (characters != std::cend(s) && digits != std::cend(s)) {
-      result.push_back(*digits);
-      result.push_back(*characters);
-    }
-  } while (characters != std::cend(s) && digits != std::cend(s));
+  };
 
-  if (result.empty()) {
-    return result;
+  // Count the frequencies
+  auto digit_count =
+    std::count_if(std::cbegin(s), std::cend(s), [](const CharTy& x) -> bool {
+      return std::isdigit(x);
+    });
+  auto character_count = s.size() - digit_count;
+
+  // Check if the difference is larger than 1, thus impossible.
+  if ((digit_count > character_count && digit_count - character_count > 1) ||
+      (character_count > digit_count && character_count - digit_count > 1)) {
+    return "";
   } else {
-    if (characters != std::cend(s)) {
+    std::string result;
+    result.reserve(s.size());
+
+    auto characters = next_characters(std::cbegin(s));
+    auto digits = next_digits(std::cbegin(s));
+
+    if (digit_count < character_count) {
+      // Put character at the front
       result.push_back(*characters);
+      characters = next_characters(std::next(characters));
+      while (digits != std::cend(s)) {
+        result.push_back(*digits);
+        result.push_back(*characters);
+        characters = next_characters(std::next(characters));
+        digits = next_digits(std::next(digits));
+      }
+    } else if (digit_count > character_count) {
+      // Put digit at the end
+      while (characters != std::cend(s)) {
+        result.push_back(*digits);
+        result.push_back(*characters);
+        characters = next_characters(std::next(characters));
+        digits = next_digits(std::next(digits));
+      }
+      result.push_back(*digits);
+    } else {
+      // March normally
+      while (digits != std::cend(s) && characters != std::cend(s)) {
+        result.push_back(*digits);
+        result.push_back(*characters);
+        characters = next_characters(std::next(characters));
+        digits = next_digits(std::next(digits));
+      }
     }
     return result;
   }
--- test/leetcode/problem_1417.cpp
+++ test/leetcode/problem_1417.cpp
@@ -28,11 +28,26 @@ TEST_CASE("problem 1346 3")
   CHECK(expected == result);
 }
 
-
 TEST_CASE("problem 1346 4")
 {
   const std::string input = "covid2019";
-  const std::string expected = "2c0o1v9id";
+  const std::string expected = "c2o0v1i9d";
+  const std::string result = leetcode::reformat(input);
+  CHECK(expected == result);
+}
+
+TEST_CASE("problem 1346 5")
+{
+  const std::string input = "j";
+  const std::string expected = "j";
+  const std::string result = leetcode::reformat(input);
+  CHECK(expected == result);
+}
+
+TEST_CASE("problem 1346 6")
+{
+  const std::string input = "2";
+  const std::string expected = "2";
   const std::string result = leetcode::reformat(input);
   CHECK(expected == result);
 }
