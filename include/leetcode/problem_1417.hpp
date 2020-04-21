fd45b72e4a6f6542de244c1c4386651828737767
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2020-04-20 22:42:42 -0400

    WIP :: A bit more work.

--- include/leetcode/problem_1417.hpp
+++ include/leetcode/problem_1417.hpp
@@ -9,12 +9,34 @@ namespace leetcode {
 static auto
 reformat(const std::string& s) -> std::string
 {
-  auto characters =
-    std::find_if(std::cbegin(s),
-                 std::cend(s),
-                 [](const typename std::string::value_type& x) -> bool {
-                   return std::isupper(x) || std::islower(x);
-                 });
-  return "";
+  using CharTy = typename std::string::value_type;
+
+  std::string result;
+  result.reserve(s.size());
+
+  auto characters = std::cbegin(s);
+  auto digits = std::cbegin(s);
+  do {
+    characters =
+      std::find_if(characters, std::cend(s), [](const CharTy& x) -> bool {
+        return std::isupper(x) || std::islower(x);
+      });
+    digits = std::find_if(digits, std::cend(s), [](const CharTy& x) -> bool {
+      return std::isdigit(x);
+    });
+    if (characters != std::cend(s) && digits != std::cend(s)) {
+      result.push_back(*digits);
+      result.push_back(*characters);
+    }
+  } while (characters != std::cend(s) && digits != std::cend(s));
+
+  if (result.empty()) {
+    return result;
+  } else {
+    if (characters != std::cend(s)) {
+      result.push_back(*characters);
+    }
+    return result;
+  }
 }
 }
--- test/leetcode/problem_1417.cpp
+++ test/leetcode/problem_1417.cpp
@@ -9,5 +9,30 @@ TEST_CASE("problem 1346 1")
   const std::string input = "a0b1c2";
   const std::string expected = "0a1b2c";
   const std::string result = leetcode::reformat(input);
-  // CHECK(expected == result);
+  CHECK(expected == result);
+}
+
+TEST_CASE("problem 1346 2")
+{
+  const std::string input = "leetcode";
+  const std::string expected = "";
+  const std::string result = leetcode::reformat(input);
+  CHECK(expected == result);
+}
+
+TEST_CASE("problem 1346 3")
+{
+  const std::string input = "123123213";
+  const std::string expected = "";
+  const std::string result = leetcode::reformat(input);
+  CHECK(expected == result);
+}
+
+
+TEST_CASE("problem 1346 4")
+{
+  const std::string input = "covid2019";
+  const std::string expected = "2c0o1v9id";
+  const std::string result = leetcode::reformat(input);
+  CHECK(expected == result);
 }
