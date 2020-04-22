f39e6c268c28862b8356609c7d0ae6d34549a04f
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2020-04-22 00:53:12 -0400

    Apparently lambdas introduce a huge cost...

--- include/april_2020_challenge/day_6.hpp
+++ include/april_2020_challenge/day_6.hpp
@@ -1,60 +1,30 @@
 #pragma once
 
 #include <algorithm>
-#include <iostream>
 #include <iterator>
-#include <map>
 #include <numeric>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 namespace april2020 {
 
 template<typename IterTy,
          typename ReturnTy = typename std::vector<std::vector<std::string>>>
-static constexpr auto
-groupAnagrams(const IterTy begin, const IterTy end) -> ReturnTy
+static auto
+groupAnagrams(IterTy begin, const IterTy end) -> ReturnTy
 {
-  // Prelude
-  using IndexTy = typename std::iterator_traits<IterTy>::difference_type;
-  using KeyTy = typename std::array<int, 26>;
-  using ValueTy = typename std::vector<IndexTy>;
-  using CharTy = typename std::string::value_type;
-  using PairTy = typename std::pair<const KeyTy, ValueTy>;
-
-  std::map<KeyTy, ValueTy> map;
-  auto iter = begin;
-  while (iter != end) {
-    KeyTy key = std::accumulate(std::cbegin(*iter),
-                                std::cend(*iter),
-                                KeyTy{},
-                                [](KeyTy& acc, const CharTy& ch) {
-                                  acc[ch - 'a']++;
-                                  return acc;
-                                });
-    auto find = map.find(key);
-    if (find == std::end(map)) {
-      map.emplace(
-        std::make_pair(key, std::vector{ std::distance(begin, iter) }));
-    } else {
-      find->second.emplace_back(std::distance(begin, iter));
-    }
-    iter = std::next(iter);
+  std::unordered_map<std::string, std::vector<std::string>> map;
+  while (begin != end) {
+    std::string t = *begin;
+    std::sort(t.begin(), t.end());
+    map[t].emplace_back(*begin);
+    begin = std::next(begin);
   }
-  return std::accumulate(
-    std::cbegin(map),
-    std::cend(map),
-    ReturnTy{},
-    [&](ReturnTy& acc, const PairTy& x) {
-      acc.emplace_back(
-        std::accumulate(std::cbegin(x.second),
-                        std::cend(x.second),
-                        std::vector<std::string>{},
-                        [&](std::vector<std::string>& acc, const IndexTy& idx) {
-                          acc.emplace_back(*std::next(begin, idx));
-                          return acc;
-                        }));
-      return acc;
-    });
+  std::vector<std::vector<std::string>> anagrams;
+  for (const auto& p : map) {
+    anagrams.emplace_back(p.second);
+  }
+  return anagrams;
 }
 }
