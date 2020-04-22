c066f00db3008cde584b01d7c3cf7c9da4e49bb3
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2020-04-21 22:17:30 -0400

    This optimization is still slow because std::map is an RB Tree
    So it needs to maintain the state with every push....

--- include/april_2020_challenge/day_6.hpp
+++ include/april_2020_challenge/day_6.hpp
@@ -2,38 +2,58 @@
 
 #include <algorithm>
 #include <iostream>
+#include <iterator>
+#include <map>
 #include <numeric>
 #include <string>
 #include <vector>
 
 namespace april2020 {
 
-template<typename IterTy>
-std::vector<std::vector<std::string>>
-groupAnagrams(IterTy begin, IterTy end)
+template<typename IterTy,
+         typename ReturnTy = typename std::vector<std::vector<std::string>>>
+static constexpr auto
+groupAnagrams(const IterTy begin, const IterTy end) -> ReturnTy
 {
+  // Prelude
+  using IndexTy = typename std::iterator_traits<IterTy>::difference_type;
+  using KeyTy = typename std::array<int, 26>;
+  using ValueTy = typename std::vector<IndexTy>;
+  using CharTy = typename std::string::value_type;
+  using PairTy = typename std::pair<const KeyTy, ValueTy>;
+
+  std::map<KeyTy, ValueTy> map;
+  auto iter = begin;
+  while (iter != end) {
+    KeyTy key = std::accumulate(std::cbegin(*iter),
+                                std::cend(*iter),
+                                KeyTy{},
+                                [](KeyTy& acc, const CharTy& ch) {
+                                  acc[ch - 'a']++;
+                                  return acc;
+                                });
+    auto find = map.find(key);
+    if (find == std::end(map)) {
+      map.emplace(
+        std::make_pair(key, std::vector{ std::distance(begin, iter) }));
+    } else {
+      find->second.emplace_back(std::distance(begin, iter));
+    }
+    iter = std::next(iter);
+  }
   return std::accumulate(
-    begin,
-    end,
-    std::vector<std::vector<std::string>>{},
-    [](std::vector<std::vector<std::string>>& acc, const std::string& x) {
-      auto sorted_x = x;
-      std::sort(std::begin(sorted_x), std::end(sorted_x));
-      auto find =
-        std::find_if(std::begin(acc),
-                     std::end(acc),
-                     [&](const std::vector<std::string>& y) -> bool {
-                       auto sorted_y = y.back();
-                       std::sort(std::begin(sorted_y), std::end(sorted_y));
-                       return sorted_x == sorted_y;
-                     });
-      if (find == std::cend(acc)) {
-        // Cannot find any match
-        acc.emplace_back(std::vector{ x });
-      } else {
-        // Match exists
-        find->emplace_back(x);
-      }
+    std::cbegin(map),
+    std::cend(map),
+    ReturnTy{},
+    [&](ReturnTy& acc, const PairTy& x) {
+      acc.emplace_back(
+        std::accumulate(std::cbegin(x.second),
+                        std::cend(x.second),
+                        std::vector<std::string>{},
+                        [&](std::vector<std::string>& acc, const IndexTy& idx) {
+                          acc.emplace_back(*std::next(begin, idx));
+                          return acc;
+                        }));
       return acc;
     });
 }
