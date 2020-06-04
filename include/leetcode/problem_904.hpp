3c521860f55e310e269912c4e908ee238e4f7883
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2020-06-03 22:53:05 -0400

    Solved problem 904 but TLE

---
+++ include/leetcode/problem_904.hpp
@@ -0,0 +1,88 @@
+#pragma once
+
+#include "util/generic/group_by.hpp"
+#include "util/generic/group_by_minify.hpp"
+#include "util/generic/group_fold.hpp"
+#include "util/generic/zip.hpp"
+#include "util/generic/zip_fold.hpp"
+
+#include <algorithm>
+#include <functional>
+#include <vector>
+
+namespace leetcode {
+
+template<typename IterTy,
+         typename ValueTy = typename std::iterator_traits<IterTy>::value_type>
+ValueTy
+totalFruit(IterTy iter_begin, IterTy iter_end)
+{
+  using KeyTy = std::pair<ValueTy, ValueTy>;
+  using FreqTy = std::size_t;
+  using FreqPairTy = std::pair<FreqTy, FreqTy>;
+  using AdjDiffTy = std::pair<KeyTy, FreqPairTy>;
+  using GroupTy = std::pair<ValueTy, FreqTy>;
+  using AdjGroupTy = std::pair<KeyTy, ValueTy>;
+
+  if (iter_begin == iter_end) {
+    // Should return Result<ValueTy>
+    throw std::runtime_error(
+      "This function cannot operate when `iter_begin == iter_end`.");
+  }
+
+  std::vector<GroupTy> group_frequency =
+    akarithm::group_by_minify(iter_begin, iter_end, std::equal_to{});
+
+  if (group_frequency.size() == 1) {
+    return group_frequency.back().second;
+  }
+
+  std::vector<AdjDiffTy> adjacent_difference = akarithm::zip_fold(
+    std::cbegin(group_frequency),
+    std::cend(group_frequency),
+    std::next(std::cbegin(group_frequency)),
+    std::cend(group_frequency),
+    std::vector<AdjDiffTy>(std::size(group_frequency)),
+    [](const GroupTy& lhs, const GroupTy& rhs) -> AdjDiffTy {
+      auto& [lhs_value, lhs_freq] = lhs;
+      auto& [rhs_value, rhs_freq] = rhs;
+      return AdjDiffTy{ KeyTy{ lhs_value > rhs_value ? rhs_value : lhs_value,
+                               lhs_value > rhs_value ? lhs_value : rhs_value },
+                        FreqPairTy{ lhs_freq, rhs_freq } };
+    },
+    [](std::vector<AdjDiffTy>& acc,
+       const AdjDiffTy& value) -> std::vector<AdjDiffTy> {
+      acc.emplace_back(value);
+      return acc;
+    });
+
+  // TODO: We need group_fold for this.
+
+  auto group_adjacent_difference_fold = akarithm::group_fold(
+    std::cbegin(adjacent_difference),
+    std::cend(adjacent_difference),
+    [](const AdjDiffTy& lhs, const AdjDiffTy& rhs) -> bool {
+      return lhs.first == rhs.first;
+    },
+    [](const std::vector<AdjDiffTy>& group) -> std::vector<FreqTy> {
+      std::vector<FreqTy> group_adjacent_difference;
+      group_adjacent_difference.reserve(group.size() + 1);
+
+      for (const AdjDiffTy& x : group) {
+        group_adjacent_difference.emplace_back(x.second.first);
+      }
+      group_adjacent_difference.emplace_back(group.back().second.second);
+      return group_adjacent_difference;
+    });
+
+  std::vector<FreqTy> max_vectors = *std::max_element(
+    std::cbegin(group_adjacent_difference_fold),
+    std::cend(group_adjacent_difference_fold),
+    [](const std::vector<FreqTy>& lhs, const std::vector<FreqTy>& rhs) -> bool {
+      return std::accumulate(std::cbegin(lhs), std::cend(lhs), 0) <
+             std::accumulate(std::cbegin(rhs), std::cend(rhs), 0);
+    });
+
+  return std::accumulate(std::cbegin(max_vectors), std::cend(max_vectors), 0);
+}
+}
